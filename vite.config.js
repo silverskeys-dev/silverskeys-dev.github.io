06ad086e9f98b30a62cf83659f7bacedb21cc0f5
Lucas Lima <lucaslimaa8play@hotmail.com>
2025-08-19 16:26:51 -0400

    deploy vite build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/silverskeys-dev.github.io/', // 👈 isso é importante
   plugins: [react()],
 })
