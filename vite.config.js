6af2bd63112af79a67e0e0610ed1e9d0938df974
silverskeys-dev <silverskeys@gmail.com>
2025-08-19 16:50:00 -0400

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/silverskeys-dev.github.io/', // 👈 isso é importante
-  plugins: [react()],
-})
