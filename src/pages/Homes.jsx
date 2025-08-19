0f1d37cfdbe1f50372bb0626e57454bd873d9336
silverskeys-dev <silverskeys@gmail.com>
2025-08-19 16:51:13 -0400

    Delete src directory

--- src/pages/Homes.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="p-8">
-      <h2 className="text-2xl font-bold mb-4">Bem-vindo ao Café da Praça</h2>
-      <p>O melhor café da cidade, ambiente acolhedor e delicioso cardápio.</p>
-    </div>
-  );
-}
